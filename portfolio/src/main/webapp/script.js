572fe437407fdbfc6da605c79635f2c190cd54b2
Hisao <jaimehisao2011@gmail.com>
2020-07-08 19:21:07 +0000

    Added Maps API

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -126,3 +126,13 @@ const addComments = async () => {
     );
   }
 };
+
+/*************************
+ *     RANDOM FACT 
+ ************************/
+function createMap(){
+  const map = new google.maps.Map(
+    document.getElementById('map'),
+    {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+  console.log('Created Map!')
+}
